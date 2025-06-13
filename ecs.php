c1e853117f03bf081e67dc9147c6618a07653b9b
Ludovic Dev <ludovicdev.io@gmail.com>
2025-06-13 02:15:07 +0200

    Add test quality code

---
+++ ecs.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Operator\ConcatSpaceFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use PhpCsFixer\Fixer\Import\GlobalNamespaceImportFixer;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests'
+    ])
+    ->withPreparedSets(true)
+    ->withSets([
+        SetList::PSR_12,
+        SetList::COMMON,
+        SetList::CLEAN_CODE,
+        SetList::PHPUNIT,
+    ])
+    ->withSkip([
+        ConcatSpaceFixer::class
+    ]);
